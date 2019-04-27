f75ba3c3c759e5941ad6ad2ba9eced46b1b78127
tivankic <tea.ivankic@redheads.de>
2019-04-27 13:21:37 +0200

    trying to commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="yApp-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
